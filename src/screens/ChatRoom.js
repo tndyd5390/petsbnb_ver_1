e800807822858295bfad349139f0c89fbb331e23
MOON <hoop_ster@naver.com>
2018-11-13 13:55:35 +0900

    18.11.13 chatroom

--- src/screens/ChatRoom.js
+++ src/screens/ChatRoom.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, FlatList, ScrollView, KeyboardAvoidingView, Keyboard, TouchableHighlight, TextInput} from 'react-native';
 import {List, ListItem} from 'react-native-elements';
+import Colors from '../utils/Colors';
 
 export default class ChatRoom extends Component{
+    _loginCheck = async() => {
+        const userInfo = await AsyncStorage.getItem('userInfo');
+        if(userInfo != null){
+          //로그인 되있는 상태
+        }
+      }
+
     static navigationOptions = ({ navigation }) => {
         return {
           title: navigation.getParam('key', 'A Nested Details Screen'),
@@ -11,8 +19,6 @@ export default class ChatRoom extends Component{
     
 
     render(){
-        const key = this.props.navigation.state.params.key;
-        const key1 = this.props;
         return(
             <View style={styles.outer}>
             <ScrollView style={styles.messages}>
@@ -28,9 +34,9 @@ class InputBar extends Component{
     render(){
         return(
             <View style={styles.inputBar}>
-                <TextInput style={styles.textBox}/>
+                <TextInput style={styles.textBox} placeholder='문자 메시지'/>
                 <TouchableHighlight style={styles.sendButton}>
-                    <Text style={{color: 'white'}}>Send</Text>
+                    <Text style={{color: 'white'}}>></Text>
                 </TouchableHighlight>
             </View>
         );
@@ -57,26 +63,28 @@ const styles = StyleSheet.create({
     inputBar: {
       flexDirection: 'row',
       justifyContent: 'space-between',
-      paddingHorizontal: 5,
-      paddingVertical: 3,
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      height:'10%'
     },
   
     textBox: {
-      borderRadius: 5,
+      borderRadius: 15,
       borderWidth: 1,
-      borderColor: 'gray',
+      borderColor: Colors.lightGrey,
+      backgroundColor : Colors.white,
       flex: 1,
-      fontSize: 16,
-      paddingHorizontal: 10
+      paddingHorizontal: 10,
     },
   
     sendButton: {
       justifyContent: 'center',
       alignItems: 'center',
+      width : '15%',
       paddingLeft: 15,
       marginLeft: 5,
       paddingRight: 15,
-      borderRadius: 5,
+      borderRadius: 15,
       backgroundColor: '#66db30'
     },
   
