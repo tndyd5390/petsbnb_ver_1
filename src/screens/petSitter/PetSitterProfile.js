368fbb0f9e77322935d116291e225e52392896d0
oohs00oohs <oohs00oohs@gmail.com>
2018-12-05 18:17:35 +0900

    펫시터 프로필 화면 완료

--- src/screens/petSitter/PetSitterProfile.js
+++ src/screens/petSitter/PetSitterProfile.js
@@ -8,6 +8,7 @@ import RNFetchBlob from 'react-native-fetch-blob';
 import Swiper from 'react-native-swiper';
 import {RadioGroup, RadioButton} from 'react-native-flexi-radio-button';
 import RoundedButton from '../components/buttons/RoundedButton';
+
 import {
     View,
     Text,
@@ -25,27 +26,10 @@ export default class PetSitterProfile extends Component{
     constructor(props){
         super(props);
         this.state = {
-            activityIndicator : false
+            activityIndicator : false,
+            tmp : '없음'
         }
     }
-    /**<View style={{ width : '75%', height : 40, borderWidth : 1, borderColor : Colors.lightGrey, borderRadius : 5}}>
-                                <Picker
-                                    selectedValue={'없음'}
-                                    onValueChange={(itemValue, itemIndex) => {}}
-                                    style={{width : '100%', height : '100%', borderWidth : 1, borderColor : Colors.black, borderRadius : 5}}
-                                    >
-                                    <Picker.Item label="없음" value="없음" />
-                                    <Picker.Item label="산책 가능" value="산책 가능" />
-                                    <Picker.Item label="목욕 가능" value="목욕 가능" />
-                                    <Picker.Item label="응급 처치" value="응급 처치" />
-                                    <Picker.Item label="모발 관리" value="모발 관리" />
-                                </Picker>
-                            </View>
-                            <View style={{width : '25%', height : 40}}>
-                                
-                            </View>
-     * 
-     */
     render(){
         return(
             <ScrollView>
@@ -80,7 +64,7 @@ export default class PetSitterProfile extends Component{
                         <View style={{width : width - 30, height : '40%'}}>
                             <Text>이름</Text>
                         </View>
-                        <View style={{width : width - 30, height : '60%'}}>
+                        <View style={{width : width - 40, height : '60%'}}>
                             <TextInput style={{backgroundColor : Colors.lightGrey, paddingLeft : 10, paddingRight : 0, paddingTop : 0, paddingBottom : 0, borderRadius : 5}}/>
                         </View>
                     </View>
@@ -89,7 +73,7 @@ export default class PetSitterProfile extends Component{
                         <View style={{width : width - 30, height : '40%'}}>
                             <Text>한줄 소개</Text>
                         </View>
-                        <View style={{width : width - 30, height : '60%'}}>
+                        <View style={{width : width - 40, height : '60%'}}>
                             <TextInput style={{backgroundColor : Colors.lightGrey, paddingLeft : 10, paddingRight : 0, paddingTop : 0, paddingBottom : 0, borderRadius : 5}}/>
                         </View>
                     </View>
@@ -98,7 +82,7 @@ export default class PetSitterProfile extends Component{
                         <View style={{width : width - 30, height : '40%'}}>
                             <Text>펫시팅 환경</Text>
                         </View>
-                        <View style={{width : width - 30, height : '60%'}}>
+                        <View style={{width : width - 40, height : '60%'}}>
                             <TextInput style={{backgroundColor : Colors.lightGrey, paddingLeft : 10, paddingRight : 0, paddingTop : 0, paddingBottom : 0, borderRadius : 5}}/>
                         </View>
                     </View>
@@ -200,6 +184,228 @@ export default class PetSitterProfile extends Component{
                         </View>
                     </View>
 
+                    <View style={{alignItems : 'center'}}>
+                        <View style={{width : width, height : 90, flexDirection : 'row'}}>
+                            <View style={{height : '100%', width : '33.333%'}}>
+                                <View style={{width : '100%', height : '70%', alignItems : 'center', justifyContent : 'center'}}>
+                                    <FontAwesome5 name='paw' color={Colors.lightGrey} size={50}/>
+                                </View>
+                                <View style={{width : '100%', height : '30%', alignItems : 'center', justifyContent : 'center'}}>
+                                    <Text>소형(1~6kg)</Text>
+                                </View>
+                            </View>
+                            <View style={{height : '100%', width : '33.333%'}}>
+                                <View style={{width : '100%', height : '70%', alignItems : 'center', justifyContent : 'center'}}>
+                                    <FontAwesome5 name='paw' color={Colors.lightGrey} size={50}/>
+                                </View>
+                                <View style={{width : '100%', height : '30%', alignItems : 'center', justifyContent : 'center'}}>
+                                    <Text>중형(7~15kg)</Text>
+                                </View>
+                            </View>
+                            <View style={{height : '100%', width : '33.333%'}}>
+                                <View style={{width : '100%', height : '70%', alignItems : 'center', justifyContent : 'center'}}>
+                                    <FontAwesome5 name='paw' color={Colors.lightGrey} size={50}/>
+                                </View>
+                                <View style={{width : '100%', height : '30%', alignItems : 'center', justifyContent : 'center'}}>
+                                    <Text>대형(15kg~)</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+
+                    
+                        
+                    <View style={{width : width, height : 45, flexDirection : 'row', marginTop : 10}}>
+                        <View style={{height : '100%', width : '33.333%', alignItems : 'center', justifyContent : 'center'}}>
+                            <View style={{ width : '95%', height : 40, borderWidth : 1, borderColor : Colors.lightGrey, borderRadius : 5}}>
+                                <Picker
+                                    selectedValue={this.state.tmp}
+                                    onValueChange={(itemValue, itemIndex) => {this.setState({tmp : itemValue})}}
+                                    style={{width : '100%', height : '100%'}}
+                                    textStyle={{fontSize : 5}}
+                                    >
+                                    <Picker.Item label="없음" value="없음" />
+                                    <Picker.Item label="10000000" value="10000000" />
+                                    <Picker.Item label="목욕 가능" value="목욕 가능" />
+                                    <Picker.Item label="응급 처치" value="응급 처치" />
+                                    <Picker.Item label="모발 관리" value="모발 관리" />
+                                </Picker>
+                            </View>
+                        </View>
+                        <View style={{height : '100%', width : '33.333%', alignItems : 'center', justifyContent : 'center'}}>
+                            <View style={{ width : '95%', height : 40, borderWidth : 1, borderColor : Colors.lightGrey, borderRadius : 5}}>
+                                <Picker
+                                    selectedValue={'없음'}
+                                    onValueChange={(itemValue, itemIndex) => {}}
+                                    style={{width : '100%', height : '100%'}}
+                                    textStyle={{fontSize : 5}}
+                                    >
+                                    <Picker.Item label="없음" value="없음" />
+                                    <Picker.Item label="10000000" value="10000000" />
+                                    <Picker.Item label="목욕 가능" value="목욕 가능" />
+                                    <Picker.Item label="응급 처치" value="응급 처치" />
+                                    <Picker.Item label="모발 관리" value="모발 관리" />
+                                </Picker>
+                            </View>
+                        </View>
+                        <View style={{height : '100%', width : '33.333%', alignItems : 'center', justifyContent : 'center'}}>
+                            <View style={{ width : '95%', height : 40, borderWidth : 1, borderColor : Colors.lightGrey, borderRadius : 5}}>
+                                <Picker
+                                    selectedValue={'없음'}
+                                    onValueChange={(itemValue, itemIndex) => {}}
+                                    style={{width : '100%', height : '100%'}}
+                                    textStyle={{fontSize : 5}}
+                                    >
+                                    <Picker.Item label="없음" value="없음" />
+                                    <Picker.Item label="산책 가능" value="산책 가능" />
+                                    <Picker.Item label="목욕 가능" value="목욕 가능" />
+                                    <Picker.Item label="응급 처치" value="응급 처치" />
+                                    <Picker.Item label="모발 관리" value="모발 관리" />
+                                </Picker>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={{width : width, height : 45, flexDirection : 'row'}}>
+                        <View style={{height : '100%', width : '33.333%', alignItems : 'center', justifyContent : 'center'}}>
+                            <View style={{ width : '95%', height : 40, borderWidth : 1, borderColor : Colors.lightGrey, borderRadius : 5}}>
+                                <Picker
+                                    selectedValue={'없음'}
+                                    onValueChange={(itemValue, itemIndex) => {}}
+                                    style={{width : '100%', height : '100%'}}
+                                    >
+                                    <Picker.Item label="없음" value="없음" />
+                                    <Picker.Item label="산책 가능" value="산책 가능" />
+                                    <Picker.Item label="목욕 가능" value="목욕 가능" />
+                                    <Picker.Item label="응급 처치" value="응급 처치" />
+                                    <Picker.Item label="모발 관리" value="모발 관리" />
+                                </Picker>
+                            </View>
+                        </View>
+                        <View style={{height : '100%', width : '33.333%', alignItems : 'center', justifyContent : 'center'}}>
+                            <View style={{ width : '95%', height : 40, borderWidth : 1, borderColor : Colors.lightGrey, borderRadius : 5}}>
+                                <Picker
+                                    selectedValue={'없음'}
+                                    onValueChange={(itemValue, itemIndex) => {}}
+                                    style={{width : '100%', height : '100%'}}
+                                    >
+                                    <Picker.Item label="없음" value="없음" />
+                                    <Picker.Item label="산책 가능" value="산책 가능" />
+                                    <Picker.Item label="목욕 가능" value="목욕 가능" />
+                                    <Picker.Item label="응급 처치" value="응급 처치" />
+                                    <Picker.Item label="모발 관리" value="모발 관리" />
+                                </Picker>
+                            </View>
+                        </View>
+                        <View style={{height : '100%', width : '33.333%', alignItems : 'center', justifyContent : 'center'}}>
+                            <View style={{ width : '95%', height : 40, borderWidth : 1, borderColor : Colors.lightGrey, borderRadius : 5}}>
+                                <Picker
+                                    selectedValue={'없음'}
+                                    onValueChange={(itemValue, itemIndex) => {}}
+                                    style={{width : '100%', height : '100%'}}
+                                    >
+                                    <Picker.Item label="없음" value="없음" />
+                                    <Picker.Item label="산책 가능" value="산책 가능" />
+                                    <Picker.Item label="목욕 가능" value="목욕 가능" />
+                                    <Picker.Item label="응급 처치" value="응급 처치" />
+                                    <Picker.Item label="모발 관리" value="모발 관리" />
+                                </Picker>
+                            </View>
+                        </View>
+                    </View>
+                    
+                    <View style={{height : 40, alignItems : 'center'}}>
+                        <View style={{width : width - 30, height : '100%', justifyContent : 'center'}}>
+                            <Text style={{fontSize : 15, color : Colors.black}}>환급계좌 정보</Text>
+                        </View>
+                    </View>
+
+                    <View style={{alignItems : 'center'}}>
+                        <View style={{width : width - 40, height : 20, flexDirection : 'row'}}>
+                            <View style={{height : '100%', width : '50%'}}>
+                                <Text>예금주명</Text>
+                            </View>
+                            <View style={{height : '100%', width : '50%'}}>
+                                <Text>은행</Text>
+                            </View>
+                        </View>
+                        <View style={{width : width - 40, height : 30, flexDirection : 'row'}}>
+                            <View style={{height : '100%', width : '50%', justifyContent : 'center'}}>
+                                <View style={{width : '90%'}}>
+                                    <TextInput style={{backgroundColor : Colors.lightGrey, paddingLeft : 10, paddingRight : 0, paddingTop : 0, paddingBottom : 0, borderRadius : 5}}/>
+                                </View>
+                            </View>
+                            <View style={{height : '100%', width : '50%', justifyContent : 'center'}}>
+                                <View style={{width : '100%'}}>
+                                    <View style={{ width : '100%', height : '100%', borderWidth : 1, borderColor : Colors.lightGrey, borderRadius : 5}}>
+                                        <Picker
+                                            selectedValue={'없음'}
+                                            onValueChange={(itemValue, itemIndex) => {}}
+                                            style={{width : '100%', height : '100%'}}
+                                            >
+                                            <Picker.Item label="선택" value="선택" />
+                                            <Picker.Item label="신한" value="신한" />
+                                            <Picker.Item label="하나" value="하나" />
+                                            <Picker.Item label="우리" value="우리" />
+                                            <Picker.Item label="농형" value="농형" />
+                                        </Picker>
+                                    </View>
+                                </View>
+                            </View>
+                        </View>
+
+                        <View style={{alignItems : 'center', marginTop : 5}}>
+                            <View style={{width : width - 40, height : 20}}>
+                                <Text>계좌번호</Text>
+                            </View>
+                            <View style={{width : width - 40, height : 30}}>
+                                <TextInput style={{backgroundColor : Colors.lightGrey, paddingLeft : 10, paddingRight : 0, paddingTop : 0, paddingBottom : 0, borderRadius : 5}}/>
+                            </View>
+                        </View>
+                        <View style={{width : width - 40}}>
+                            <Text style={{color : Colors.red}}>* 계좌번호를 정확히 입력해 주세요.</Text>
+                        </View>
+                    </View>
+
+                    <View style={{height : 40, alignItems : 'center'}}>
+                        <View style={{width : width - 30, height : '100%', justifyContent : 'center'}}>
+                            <Text style={{fontSize : 15, color : Colors.black}}>지참 항목</Text>
+                        </View>
+                    </View>
+                    <View style={{alignItems : 'center'}}>
+                        <View style={{width : width - 30, height : 100}}>
+                            <TextInput 
+                                style={{width : '100%', height : '100%', backgroundColor : Colors.lightGrey, borderRadius : 5}}
+                                multiline={true}
+                            />
+                        </View>
+                    </View>
+
+                    <View style={{height : 40, alignItems : 'center'}}>
+                        <View style={{width : width - 30, height : '100%', justifyContent : 'center'}}>
+                            <Text style={{fontSize : 15, color : Colors.black}}>펫시터 소개</Text>
+                        </View>
+                    </View>
+                    <View style={{alignItems : 'center'}}>
+                        <View style={{width : width - 30, height : 100}}>
+                            <TextInput 
+                                style={{width : '100%', height : '100%', backgroundColor : Colors.lightGrey, borderRadius : 5}}
+                                multiline={true}
+                            />
+                        </View>
+                    </View>
+                    <View style={{marginTop : 10}}>
+                        <TouchableOpacity 
+                            style={{width: width, 
+                            height: 50, 
+                            backgroundColor: Colors.buttonSky, 
+                            justifyContent: 'center', 
+                            alignItems: 'center'}}
+                            onPress={this._nextStep}
+                        >
+                            <Text style={{color : Colors.white, fontSize : 20, fontWeight : '700'}}>등록</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </ScrollView>
         );
